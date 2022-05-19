b699bdd1df24c0af7d8374157233c998a8208a16
yehudz <yehuda.zeytim@gmail.com>
2022-05-19 11:53:40 -0600

    fix: Add feedback button for mobile view

--- pages/components/MobileTopBar/MobileOptionsMenu.tsx
+++ pages/components/MobileTopBar/MobileOptionsMenu.tsx
@@ -1,15 +1,18 @@
-import { Typography } from "@mui/material"
 import MobileNavStyles from '../../../styles/MobileNavStyles.module.scss'
 import DropDownMenu from "../DropDownMenu"
 import PrimaryButton from "../buttons/PrimaryButton"
-import { Plus } from "phosphor-react"
 
 export const MobileOptionsMenu = ()=> {
   const menuItems = ['Most Upvotes', 'Least Upvotes', 'Most Comments', 'Least Comments']
   return (
     <div className={MobileNavStyles.optionsMenuContainer}>
       <DropDownMenu menuItems={menuItems} sortDropdown={true} mobile={true}/>
-      <PrimaryButton variant="contained" startIcon={<Plus size={14} weight="bold"/>}>Add Feedback</PrimaryButton>
+      <PrimaryButton 
+        variant="contained" 
+        sx={{height: '40px', width: '135px', fontSize: '13px'}}
+      >
+        + Add Feedback
+      </PrimaryButton>
     </div>
   )
 }
--- pages/components/buttons/PrimaryButton.tsx
+++ pages/components/buttons/PrimaryButton.tsx
@@ -1,6 +1,8 @@
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
+let mobileView: boolean
+
 const PrimaryButton = styled(Button)(({ theme }) => ({
   height: '44px',
   width: '158px',
