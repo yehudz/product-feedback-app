c1b69891d814bb234705214164a0b63642164420
yehudz <yehuda.zeytim@gmail.com>
2022-06-15 11:30:28 -0600

    feat: Add Roadmap panel

---
+++ pages/components/RoadmapPanel/RoadmapPanel.tsx
@@ -0,0 +1,81 @@
+import { Box, Typography, Grid } from '@mui/material';
+import PanelStyles from '../../../styles/Panel.module.scss';
+import GoBackBtnLight from '../buttons/GoBackBtnLight';
+
+export const RoadmapPanel = ()=> {
+
+  // Dummy data
+  const roadmapList = [
+    {
+      color: '#F49F85',
+      title: 'Planned',
+      amount: 2
+    },
+    {
+      color: '#AD1FEA',
+      title: 'In-Progress',
+      amount: 3
+    },
+    {
+      color: '#62BCFA',
+      title: 'Live',
+      amount: 1
+    }
+  ]
+
+  return(
+    <Box className={PanelStyles.container}>
+      <Grid 
+        container 
+        alignItems={'flex-start'} 
+        justifyContent="space-between"
+        mb={1}
+      >
+        <Typography variant='h3' color="success.main">Roadmap</Typography>
+        <GoBackBtnLight 
+          sx={
+              {
+                width: '8px', 
+                padding: 0, 
+                justifyContent: 'flex-end', 
+                alignItems: 'flex-start',
+                color: 'secondary.main',
+                fontWeight: '300',
+                textDecoration: 'underline'
+              }
+              }
+            >
+              View
+        </GoBackBtnLight>
+      </Grid>
+      <Grid container>
+        <div className={PanelStyles.roadmapList}>
+          {roadmapList.map(item=> {
+            return(
+              <>
+                <div 
+                  className={PanelStyles.dot} 
+                  style={{backgroundColor: item.color}}
+                ></div>
+                <Typography 
+                  variant='body1' 
+                  color="success.light"
+                >
+                  {item.title}
+                </Typography>
+                <Typography 
+                  variant='body1'
+                  fontWeight={600} 
+                  color='success.light'
+                  textAlign={'right'}
+                >
+                  {item.amount}
+                </Typography>
+              </>
+            )
+          })}
+        </div>
+      </Grid>
+    </Box>
+  )
+}
